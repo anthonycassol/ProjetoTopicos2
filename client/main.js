8aac29d272a30ea74d8bd072ec42c92ac66f9306
anthonycassol <anthonylcassol@gmail.com>
2017-07-07 04:36:35 -0300

    Primeiro commit

---
+++ client/main.js
@@ -0,0 +1,67 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+import '../imports/banco.js';
+
+
+
+Router.configure({
+  layoutTemplate: 'index',
+  layoutTemplate: 'carrinho'
+});
+
+Router.route('/', function () {
+  this.layout('index');
+});
+
+Router.route('/carrinho', function () {
+  this.layout('carrinho');
+});
+
+
+
+Template.imagens.events({
+   'click #mostra_janela': function(events){
+	$("#modal_form").modal('show');
+   },
+   'click #deleta_imagem': function(events){
+	    Images.remove(this._id); 
+   }
+});
+
+Template.imagens.helpers({
+	images: function(){        
+    return Images.find({});
+  }
+});
+
+
+Template.cart.events({
+   'click #deleta_imagem': function(events){
+	    Images.remove(this._id); 
+   }
+});
+
+Template.cart.helpers({
+  images: function(){        
+    return Images.find({});
+  }
+}); 	
+  
+
+Template.janela_cadastro.events({
+    'submit #form_cadastro': function(events){
+	Images.insert({src: $("#src").val(), title: $("#title").val(), price: $("#price").val(), alt: $("#alt").val(), well: $("#well").val()});
+	 
+    }
+});
+
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+});
+
+
+
+
+
